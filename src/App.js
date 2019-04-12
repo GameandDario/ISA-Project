3b983fd392bbea30c2ec5c778ab2af2b643c5865
Dan CARBONE <dan.carbone@live.fr>
2019-04-12 10:43:23 +0200

    Clean code of App js/css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
+import Stream from './components/Stream/Stream';
 import MapApi from './components/Map/Mapapi';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import './components/Map/Map.css';
-import Stream from './components/Stream/Stream';
+import './App.css';
 
 class App extends Component {
   render() {
