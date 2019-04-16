07339c36c3b63b7c14f35365d7b017f619f6801e
JeanLab <jeanlabedwcs@gmail.com>
2019-04-16 17:03:41 +0200

    add background and style to zoom button

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 import { Map, Marker, TileLayer } from 'react-leaflet';
 import iconPerson from './Icon';
 
-const LeafletMap = ({lat, lon}) => {
-  
-  const position = [lon, lat];
-  
-  return (
-    <Map center={position} zoom={5}
+const LeafletMap = ({lat, lon, zoom, updateZoom}) => {
 
-      onViewportChange={(e) => { console.log(e);}
-      }>
+  const position = [lon, lat];
+
+  return (
+    <Map center={position} zoom={zoom}
+
+      onViewportChange={(e) => {updateZoom (e.zoom);}}>
+      
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+        url='https://{s}.tiles.mapbox.com/v3/mapbox.blue-marble-topo-bathy-jul/{z}/{x}/{y}.png'
       />
       <Marker position={position}
-        icon = {iconPerson} className='rotatemarker'>
+        icon = {iconPerson}>
           
       </Marker>
     </Map>
