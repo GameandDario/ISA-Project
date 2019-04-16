12e3d1950132063c651cdccfc400f3295af40425
Clea Godji <clea.godji@gmail.com>
2019-04-16 17:57:59 +0100

    commit to pull footer

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import MapApi from './components/Map/Mapapi';
 import 'bootstrap/dist/css/bootstrap.css';
 import './components/Map/Map.css';
 import Stream from './components/Stream/Stream';
-
+import AboutUs from './components/About/aboutUs';
+import ModalExample from './components/Form/modalForm';
+import Footer from './components/Footer/footer';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <MapApi />
         <Stream />
+        <AboutUs />
+        <ModalExample />
+        <Footer />
       </div>
     );
   }
