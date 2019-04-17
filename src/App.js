525bf94a4b551f84b012bc40a61c302924e0a36f
Dan CARBONE <dan.carbone@live.fr>
2019-04-17 21:47:44 +0200

    Modified: App.js(clean code)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import Stream from './components/Stream/Stream';
 import MapApi from './components/Map/Mapapi';
+import Footer from './components/Footer/footer';
+import Header from './components/Header/Header';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEnvelope, faKey, faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './components/Map/Map.css';
