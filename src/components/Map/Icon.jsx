66f064faa59b963586666d70ad09b0428eb1c771
Urth <dragondor@protonmail.com>
2019-04-24 11:37:53 +0200

    fix with lint

--- src/components/Map/Icon.jsx
+++ src/components/Map/Icon.jsx
@@ -1,16 +1,8 @@
 import L from 'leaflet';
 
 const iconPerson = new L.Icon({
-<<<<<<< HEAD:src/components/Map/Icon.js
-  // iconUrl=IssMarker,
-  // iconRetinaUrl=IssMarker, 
-
-  iconUrl: require('./Marker.png'),
-  iconRetinaUrl: require('./Marker.png'),
-=======
   iconUrl: require('./Marker2.png'),
   iconRetinaUrl: require('./Marker2.png'),
->>>>>>> 8d54aa05dbea9368c2e293eb67c3cf676eb739f0:src/components/Map/Icon.jsx
   iconAnchor: (25, 34),
   popupAnchor: null,
   shadowUrl: null,
