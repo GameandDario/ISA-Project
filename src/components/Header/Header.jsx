44c234b3ca0a113e55257580c35bf6c7d91fe668
Dan CARBONE <dan.carbone@live.fr>
2019-04-25 16:24:34 +0200

    Je commit pour pull dev

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,10 +4,11 @@ import {
   Navbar,
   NavbarToggler,
   NavbarBrand,
-  Nav
+  Nav,
+  NavItem,
+  NavLink
 } from 'reactstrap';
 import './header.css';
-import FooterinHeader from './FooterinHeader';
 
 export default class Header extends React.Component {
   constructor(props) {
@@ -27,15 +28,18 @@ export default class Header extends React.Component {
     return (
       <div>
         <Navbar className="mainNavBar navbar-dark reflection" expand="md">
-          <img src="https://trello-attachments.s3.amazonaws.com/5ca317fe74fb540c9a1cb88b/5ca3812724de1a74acaa7030/a70647b2759f978c83fe1dcfb941f303/LOGO_ISA.png" 
+          <img src="https://trello-attachments.s3.amazonaws.com/5ca317fe74fb540c9a1cb88b/5ca3812724de1a74acaa7030/a70647b2759f978c83fe1dcfb941f303/LOGO_ISA.png"
             className="IsaLogo" alt="IsaLogo" />
           <h1><NavbarBrand className="brand" href="/">SPOT THE ISS </NavbarBrand></h1>
           <NavbarToggler className="bg-dark light navbar-toggler" onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav navbar>
-            
-              <FooterinHeader className="navbar-toggler"/>
-              
+            <Nav navbar className="navbar-toggler">
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+              </NavItem>
             </Nav>
           </Collapse>
         </Navbar>
