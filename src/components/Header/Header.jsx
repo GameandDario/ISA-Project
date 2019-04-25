ff2bd66e7fe56127439e549818c21d6fc1c431ff
Urth <dragondor@protonmail.com>
2019-04-25 16:47:36 +0200

    header.jsx fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,11 +3,19 @@ import {
   Collapse,
   Navbar,
   NavbarToggler,
-  NavbarBrand,
+  NavbarBrand, NavLink, NavItem,
   Nav
 } from 'reactstrap';
 import './header.css';
-import FooterinHeader from './FooterinHeader';
+import icon from '../Footer/icon.png';
+import AboutUs from '../About/aboutUs';
+import YouKnow from '../DoYouKnow/youKnow';
+import LegalMention from '../LegalMention/legalMention';
+import youtube from '../Footer/youtube.png';
+import linkedin from '../Footer/linkedin.png';
+import twitter from '../Footer/twitter.png';
+import facebook from '../Footer/facebook.png';
+
 
 export default class Header extends React.Component {
   constructor(props) {
@@ -27,15 +35,14 @@ export default class Header extends React.Component {
     return (
       <div>
         <Navbar className="mainNavBar navbar-dark reflection" expand="md">
-          <img src="https://trello-attachments.s3.amazonaws.com/5ca317fe74fb540c9a1cb88b/5ca3812724de1a74acaa7030/a70647b2759f978c83fe1dcfb941f303/LOGO_ISA.png" 
+          <img src="https://trello-attachments.s3.amazonaws.com/5ca317fe74fb540c9a1cb88b/5ca3812724de1a74acaa7030/a70647b2759f978c83fe1dcfb941f303/LOGO_ISA.png"
             className="IsaLogo" alt="IsaLogo" />
           <h1><NavbarBrand className="brand" href="/">SPOT THE ISS </NavbarBrand></h1>
           <NavbarToggler className="bg-dark light navbar-toggler" onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav navbar>
-            
-              <FooterinHeader className="navbar-toggler"/>
+            <Nav navbar className="navbar-toggler">
               
+
             </Nav>
           </Collapse>
         </Navbar>
