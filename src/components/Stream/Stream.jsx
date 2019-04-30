8966937a8384dd47a05131175aff79e121fba8af
JeanLab <jeanlabedwcs@gmail.com>
2019-04-30 10:39:35 +0200

    styl to marker

--- src/components/Stream/Stream.jsx
+++ src/components/Stream/Stream.jsx
@@ -27,26 +27,27 @@ export default class Stream extends Component {
         <Container className="videoLayout">
           <Row className='videoBorder'>
             {/* Call react-simple-video */}
-            <VideoPlayer url={this.state.videoUrl} autoplay />
+            <VideoPlayer url={this.state.videoUrl} height='156'
+              autoplay />
             
             <ButtonGroup vertical className="videoLayout">
               <Button className="videoLayout"
                 color="dark" 
                 onClick={() => this.onRadioBtnClick('video1', 'https://www.youtube.com/watch?v=Xjs6fnpPWy4')} 
                 disabled={this.state.currentVideo === 'video1'}
-              >Video 1</Button>
+              >1</Button>
 
               <Button 
                 color="dark"
                 onClick={() => this.onRadioBtnClick('video2', 'https://www.youtube.com/watch?v=4993sBLAzGA')} 
                 disabled={this.state.currentVideo === 'video2'}
-              >Video 2</Button>
+              >2</Button>
                                     
               <Button
                 color="dark" 
                 onClick={() => this.onRadioBtnClick('video3', 'https://www.youtube.com/watch?v=21X5lGlDOfg')} 
                 disabled={this.state.currentVideo === 'video3'}
-              >Video 3</Button>
+              >3</Button>
             </ButtonGroup>
           </Row>
         </Container>
